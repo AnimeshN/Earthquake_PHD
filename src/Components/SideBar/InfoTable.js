63338f3ac5c840e018878bf74c840cb93e040910
AnimeshN <animesh.nauti@gmail.com>
2020-12-14 20:11:15 +0530

    final

--- src/Components/SideBar/InfoTable.js
+++ src/Components/SideBar/InfoTable.js
@@ -1,7 +1,13 @@
-import {TableContainer, TableHead, 
-    TableBody, TableCell,Table,TableRow} from '@material-ui/core'
+import {useState} from 'react';
+import {Select,MenuItem,TableContainer, TableHead, 
+    TableBody, TableCell,Table,TableRow, FormControl} from '@material-ui/core'
 
-export const InfoTable = ({location}) => {
+export const InfoTable = ({location,setLocation}) => {
+    const handleChange = (event) =>{
+        const name = event.target.name;
+        setLocation({...location,[name]:event.target.value})
+    }
+    if(location){
     return (
         <TableContainer>
         <Table>
@@ -17,43 +23,164 @@ export const InfoTable = ({location}) => {
                     <TableCell> {location?.Village}</TableCell>
                 </TableRow>
                 <TableRow>
-                    <TableCell> Rainfall</TableCell>
-                    <TableCell> {location?.Rainfall}</TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell> River Bank failure</TableCell>
-                    <TableCell> {location?.River_Bank_failure}</TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell> Overburden Depth</TableCell>
-                    <TableCell> {location?.Overburden_Depth}</TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell> Road Influence</TableCell>
-                    <TableCell> {location?.Road_Influence}</TableCell>
-                </TableRow>
-                <TableRow>
                     <TableCell> Rock State</TableCell>
-                    <TableCell> {location?.Rockstate}</TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell> Toe failure</TableCell>
-                    <TableCell> {location?.Toe_failure}</TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell> Weathering</TableCell>
-                    <TableCell> {location?.Weathering}</TableCell>
-                </TableRow>
-                <TableRow>
-                    <TableCell> Erosion</TableCell>
-                    <TableCell> {location?.Erosion}</TableCell>
+                    <TableCell>
+                        <Select value={location?.Rockstate} onChange={handleChange} inputProps={{name:'Rockstate'}}>
+                            <MenuItem value="Fractured">Fractured</MenuItem>
+                            <MenuItem value="sheared">Sheared</MenuItem>
+                            <MenuItem value="massive">Massive</MenuItem>
+                            <MenuItem value="jointed">Jointed</MenuItem>
+                        </Select>
+                    </TableCell>
                 </TableRow>
                 <TableRow>
                     <TableCell> Hydrology</TableCell>
-                    <TableCell> {location?.Hydrology}</TableCell>
+                    <TableCell>
+                        <Select value={location?.Hydrology} onChange={handleChange} inputProps={{name:'Hydrology'}}>
+                            <MenuItem value="Dry">Dry</MenuItem>
+                            <MenuItem value="Wet">Wet</MenuItem>
+                            <MenuItem value="Damp">Damp</MenuItem>
+                        </Select>
+                    </TableCell>
                 </TableRow>
+                <TableRow>
+                    <TableCell> Weathering</TableCell>
+                    <TableCell>
+                        <Select value={location?.Weathering} onChange={handleChange} inputProps={{name:'Weathering'}}>
+                            <MenuItem value="high">High</MenuItem>
+                            <MenuItem value="Moderate">Moderate</MenuItem>
+                            <MenuItem value="low">Low</MenuItem>
+                        </Select>
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell> Overburden Depth</TableCell>
+                    <TableCell> 
+                        <FormControl>
+                            <Select value={location?.Overburden_Depth} onChange={handleChange} inputProps={{name:'Overburden_Depth'}}>
+                                <MenuItem value="0-1m">0-1m</MenuItem>
+                                <MenuItem value="1-2m">1-2m</MenuItem>
+                                <MenuItem value="2-5m">2-5m</MenuItem>
+                                <MenuItem value=">5m">>5m</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell> Erosion</TableCell>
+                    <TableCell>
+                            <Select value={location?.Erosion} onChange={handleChange} inputProps={{name:'Erosion'}}>
+                                <MenuItem value="yes">Yes</MenuItem>
+                                <MenuItem value="no">No</MenuItem>
+                            </Select>
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell> Rainfall</TableCell>
+                    <TableCell> 
+                    <FormControl>
+                        <Select value={location?.Rainfall} onChange={handleChange} inputProps={{name:'Rainfall'}}>
+                            <MenuItem value="yes">Yes</MenuItem>
+                            <MenuItem value="no">No</MenuItem>
+                        </Select>
+                    </FormControl>
+                  
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell> Road Influence</TableCell>
+                    <TableCell>
+                        <Select value={location?.Road_Influence} onChange={handleChange} inputProps={{name:'Road_Influence'}}>
+                            <MenuItem value="yes">Yes</MenuItem>
+                            <MenuItem value="no">No</MenuItem>
+                        </Select>
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell> Joint Failure</TableCell>
+                    <TableCell>
+                    <Select value={location?.Joint_failure} onChange={handleChange} inputProps={{name:'Joint_failure'}}>
+                            <MenuItem value="yes">Yes</MenuItem>
+                            <MenuItem value="no">No</MenuItem>
+                        </Select>
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell> River Bank failure</TableCell>
+                    <TableCell> 
+                    <FormControl>
+                        <Select value={location?.River_Bank_failure} onChange={handleChange} inputProps={{name:'River_Bank_failure'}}>
+                            <MenuItem value="yes">Yes</MenuItem>
+                            <MenuItem value="no">No</MenuItem>
+                        </Select>
+                    </FormControl>
+                    
+                    </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell> Toe failure</TableCell>
+                    <TableCell>
+                        <Select value={location?.Toe_failure} onChange={handleChange} inputProps={{name:'Toe_failure'}}>
+                            <MenuItem value="yes">Yes</MenuItem>
+                            <MenuItem value="no">No</MenuItem>
+                        </Select></TableCell>
+                </TableRow>
+           
+                
             </TableBody>
         </Table>
     </TableContainer>
     )
-}
+    }else{ return <TableContainer>
+        <Table>
+            <TableHead>
+                <TableRow>
+                    <TableCell>Parameters</TableCell>
+                    <TableCell>Value</TableCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                <TableRow>
+                    <TableCell> Village Name</TableCell>
+                    <TableCell> </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell> Rainfall</TableCell>
+                    <TableCell> </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell> River Bank failure</TableCell>
+                    <TableCell></TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell> Overburden Depth</TableCell>
+                    <TableCell> </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell> Road Influence</TableCell>
+                    <TableCell> </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell> Rock State</TableCell>
+                    <TableCell> </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell> Toe failure</TableCell>
+                    <TableCell> </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell> Weathering</TableCell>
+                    <TableCell> </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell> Erosion</TableCell>
+                    <TableCell> </TableCell>
+                </TableRow>
+                <TableRow>
+                    <TableCell> Hydrology</TableCell>
+                    <TableCell> </TableCell>
+                </TableRow>
+            </TableBody>
+        </Table>
+    </TableContainer>
+    }}
--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,25 +1,47 @@
+import {useEffect,useState} from 'react'
 import {FormControl, Select, MenuItem,InputLabel} from '@material-ui/core';
-
 import {SusceptibilityAlert} from "./SusceptibilityAlert";
-
 import {InfoTable} from "./InfoTable";
 
 
 import "./SideBar.scss"
 export const  SideBar = ({data, selVillage, setSelVillage,location,setLocation}) => {
     
+    const [sus, setSus] = useState(null);
+    // setSus(location?.Susceptibility);
+
     const handleChange = e =>{
         let village_id = e.target.value;
         setSelVillage(village_id);
         setLocation(data.filter(d => d.ID === village_id)[0])
       }
 
-    console.log(location)
+    
+    useEffect(()=>{
+        if(location){
+            const arg1 = location.Rockstate 
+            const arg2 = location.Hydrology	
+            const arg3 = location.Weathering	
+            const arg4 = location.Overburden_Depth	
+            const arg5 = location.Erosion	
+            const arg6 = location.Rainfall	
+            const arg7 = location.Road_Influence	
+            const arg8 = location.Joint_failure	
+            const arg9 = location.River_Bank_failure	
+            const arg10 = location.Toe_failure
+    
+            const url = `http://localhost:8000/api/${arg1}/${arg2}/${arg3}/${arg4}/${arg5}/${arg6}/${arg7}/${arg8}/${arg9}/${arg10}`;
+            fetch(url)
+            .then(response => response.json())
+            .then(json => setSus(json.Susceptibility));
+
+        }
+    },[location])
 
 
     return(
     <div id="sidebar">
-    <FormControl style={{width:"100%"}}>
+    <FormControl  style={{width:"100%"}}>
       <InputLabel id="demo-simple-select-label">Select Location</InputLabel>
       <Select
         labelId="demo-simple-select-label"
@@ -29,13 +51,15 @@ export const  SideBar = ({data, selVillage, setSelVillage,location,setLocation})
         onChange={handleChange}
       >
         {data.map(d => (
-        <MenuItem value={d.ID}>{d.ID}</MenuItem>
+        <MenuItem key={d.ID} value={d.ID}>{d.ID}</MenuItem>
         ))}
       </Select>
+      </FormControl>
 
-        <InfoTable location={location}/>
-        <SusceptibilityAlert value={location?.Susceptibility}/>
+        <SusceptibilityAlert value={sus}/>
+        <div id="infotable">
+            <InfoTable location={location} setLocation={setLocation}/>
+        </div>
 
-    </FormControl>
     </div>
 )}
